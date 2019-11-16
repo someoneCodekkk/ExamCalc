f461bb1ae50da03d41f4638a7e43a0dc9bf4b8b7
igor klipkov <igorkoklipkov@gmail.com>
2019-11-16 15:34:43 +0200

    Alalizer and Calc class

---
+++ AnalizerClass/AnalizerClass/Class1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnalizerClass
+{
+    public class AnilizerClassDll
+    {
+        // Приймає стрічку прикладу яку потрібно перевірити на коректність
+        static bool Validation(string str)
+        {
+            bool b = false;
+            int v = 0, z = 0;
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                if (char.IsDigit(str[i])) b = true;
+                if (i == str.Length - 1 && b == false) return b;
+                if (!char.IsDigit(str[i]) && str[i] != ')' && str[i + 1] == '\0') return false;
+                if (!char.IsDigit(str[i]) && str[i + 1] == '0' && !char.IsDigit(str[i])) return false;
+                if (char.IsDigit(str[i]) && str[i + 1] == '(') return false;
+                if (str[i] == ')' && char.IsDigit(str[i + 1])) return false;
+                if (str[i] == '(') v++;
+                if (str[i] == ')') z++;
+                if (str[i] == '/' && str[i + 1] == '0') return false;
+                if (str[i] == '.' && i == 0) return false;
+                if (str[i] == '.' && Char.IsDigit(str[i + 1]) != true || Char.IsDigit(str[i - 1]) != true) return false;
+                if (str[i] != '(' && str[i] != ')' && !char.IsDigit(str[i]) && str[i] != '+' && str[i] != '/' && str[i] != '*' && str[i] != '-' && str[i] != '.') { return false; }
+                if (z > v) return false;
+
+
+            }
+
+
+            if (v != z) return false;
+            return true;
+        }
+
+
+
+        //Робить реверс стрічки викликається у методі ToPolandString
+        public static string ReverceString(string input)
+        {
+            string output = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                output += input[i];
+            }
+
+            return output;
+
+        }
+
+        //Приймає приклад у вигляді правльної стрічки стрічки перед цим меодом викликати метод Validation якщо результат true викликати цей метод
+        public static string ToPolandString(string str)
+        {
+            Stack<char> operation = new Stack<char>();
+            Stack<char> nums = new Stack<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+
+                if (char.IsDigit(str[i]) || str[i] == '.') nums.Push(str[i]);
+                else
+                {
+                    nums.Push(' ');
+                    if (str[i] == ')')
+                    {
+
+                        while (true)
+                        {
+                            if (operation.Peek() == '(') { operation.Pop(); break; }
+                            else
+                            {
+                                nums.Push(operation.Pop());
+                            }
+
+                        }
+
+
+
+                    }
+
+
+                    else
+                    {
+                        if (str[i] == '(') operation.Push(str[i]);
+                        else
+                        {
+                            if (operation.Count == 0) operation.Push(str[i]);
+                            else
+                            {
+                                if ((operation.Peek() == '+' || operation.Peek() == '-') && (str[i] == '*' || str[i] == '/')) operation.Push(str[i]);
+                                else
+                                {
+                                    if (operation.Peek() == '(') operation.Push(str[i]);
+                                    else
+                                    {
+
+                                        if ((operation.Peek() == '*' || operation.Peek() == '/') && (str[i] == '*' || str[i] == '/')) nums.Push(operation.Pop());
+
+                                        else
+                                        {
+                                            while (true)
+                                            {
+                                                if (operation.Count == 0) break;
+                                                else
+                                                {
+                                                    if (operation.Peek() == '(') break;
+                                                    else
+                                                    {
+                                                        nums.Push(operation.Pop());
+                                                    }
+                                                }
+
+
+
+                                            }
+                                        }
+
+
+                                        operation.Push(str[i]);
+                                    }
+
+
+
+                                }
+
+
+
+
+
+
+
+
+
+
+                            }
+                        }
+
+
+
+                    }
+                }
+            }
+
+
+            while (operation.Count != 0)
+            {
+
+                nums.Push(operation.Pop());
+            }
+            string PolandString = "";
+
+
+            while (nums.Count != 0)
+            {
+                PolandString += nums.Pop().ToString();
+
+            }
+
+
+
+
+
+            return ReverceString(PolandString);
+
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ MathClass/MathClass/Class1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClass
+{
+    //Допоміжний клас для спрощення обрахунку
+    public class NumberOrSymbol
+    {
+        public double Number { set; get; }
+        public char Operation { set; get; }
+        public void SetValue(double val)
+        {
+
+            Number = val;
+
+        }
+
+    }
+
+
+    public class MathClassDll
+    {
+        // Метод який переводить символи в дабл приймає стрічку прикладу та позицію з якої потрібно считувати число
+        public static double CharToDouble(string str, ref int i)
+        {
+            string number = "";
+
+            while (char.IsDigit(str[i]) || str[i] == '.')
+            {
+                number += str[i].ToString();
+
+                i++;
+
+            }
+
+            number = number.Replace('.', ',');
+
+
+
+
+            return double.Parse(number);
+
+        }
+
+
+        //Метод який рахує результат прикладу приймає польський вираз 
+        public static double Calculation(string str)
+        {
+
+            List<NumberOrSymbol> nums = new List<NumberOrSymbol>();
+
+
+            double num;
+            for (int i = 0; i < str.Length;)
+            {
+                if (str[i] == '-' || str[i] == '*' || str[i] == '+' || str[i] == '/')
+                {
+                    nums.Add(new NumberOrSymbol { Operation = str[i] });
+                    i++;
+                }
+                else
+                {
+
+                    if (char.IsDigit(str[i]))
+                    {
+                        num = CharToDouble(str, ref i);
+                        nums.Add(new NumberOrSymbol { Number = num });
+
+                    }
+                    else { i++; };
+                }
+            }
+
+
+            bool b = true;
+
+
+
+
+
+            while (nums.Count != 1)
+            {
+
+                for (int i = 0; i < nums.Count; i++)
+                {
+                    if (nums[i].Operation == '+' || nums[i].Operation == '-' || nums[i].Operation == '/' || nums[i].Operation == '*') Console.Write(nums[i].Operation);
+                    else
+                    {
+                        Console.Write(nums[i].Number);
+                    }
+                }
+                Console.WriteLine();
+
+
+
+
+
+                for (int i = 0; i < nums.Count; i++)
+                {
+
+
+                    if (nums[i].Operation == '+')
+                    {
+                        nums[i - 2].SetValue(nums[i - 2].Number + nums[i - 1].Number);
+                        b = false;
+
+
+                    }
+                    if (nums[i].Operation == '-')
+                    {
+                        nums[i - 2].SetValue(nums[i - 2].Number - nums[i - 1].Number);
+                        b = false;
+
+
+
+                    }
+                    if (nums[i].Operation == '*')
+                    {
+                        nums[i - 2].SetValue(nums[i - 2].Number * nums[i - 1].Number);
+                        b = false;
+
+
+                    }
+                    if (nums[i].Operation == '/')
+                    {
+                        nums[i - 2].SetValue(nums[i - 2].Number / nums[i - 1].Number);
+                        b = false;
+
+
+                    }
+                    if (b == false)
+                    {
+                        b = true;
+                        nums.Remove(nums[i]);
+                        nums.Remove(nums[i - 1]);
+                        break;
+                    }
+                }
+
+            }
+
+            return nums[0].Number;
+        }
+
+
+
+    }
+}
